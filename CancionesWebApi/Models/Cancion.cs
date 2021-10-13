ff7756355d6458ac165fb2e235748bffdbbe734f
Andrea <a2020122545@estudiantes.upsa.edu.bo>
2021-10-12 21:11:04 -0400

    cambios en el model

--- CancionesWebApi/Models/Cancion.cs
+++ CancionesWebApi/Models/Cancion.cs
@@ -11,6 +11,7 @@ namespace CancionesWebApi.Models
         [Key]
         public string Nombre { get; set; }
         [Required(AllowEmptyStrings = false, ErrorMessage = "{0} es requerido")]
+        [StringLength(200, MinimumLength = 10, ErrorMessage = "La longitud de {0} debe estar entre {2} y {1}")]
         public string Autor { get; set; }
         [Required(AllowEmptyStrings = false, ErrorMessage = "{0} es requerido")]
         [StringLength(200, MinimumLength = 10, ErrorMessage = "La longitud de {0} debe estar entre {2} y {1}")]
