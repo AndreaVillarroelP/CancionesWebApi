e35ed9fdfa06be04e13cf15f9a6fa702c318b749
Andrea <a2020122545@estudiantes.upsa.edu.bo>
2021-11-14 23:28:40 -0400

    Authentications creadas, User creado y appsettings modificado

---
+++ CancionesWebApi/Models/AuthenticateResponse.cs
@@ -0,0 +1,25 @@
+﻿using CancionesWebApi.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CancionesWebApi.Models
+{
+    public class AuthenticateResponse
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        public string Token { get; set; }
+        public AuthenticateResponse(User user,string token)
+        {
+            Id = user.Id;
+            FirstName = user.FirstName;
+            LastName = user.LastName;
+            UserName = user.LastName;
+            Token = Token;
+        }
+    }
+}
