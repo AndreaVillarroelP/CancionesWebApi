d4049c48980fb65298222eaad215a6ce5b4de653
Andrea <a2020122545@estudiantes.upsa.edu.bo>
2021-11-15 02:04:54 -0400

    JwtMiddleware modificado

--- CancionesWebApi/Helpers/JwtMiddleware.cs
+++ CancionesWebApi/Helpers/JwtMiddleware.cs
@@ -32,7 +32,7 @@ namespace CancionesWebApi.Helpers
             {
                 var tokenHandler = new JwtSecurityTokenHandler();
                 var key = Encoding.ASCII.GetBytes(_appSettings.Secret);
-                tokenHandler.ValidateToken(token, new Microsoft.IdentityModel.Tokens.TokenValidationParameters)
+                tokenHandler.ValidateToken(token, new Microsoft.IdentityModel.Tokens.TokenValidationParameters
                 {
                     ValidateIssuerSigningKey = true,
                     IssuerSigningKey = new SymmetricSecurityKey(key),
@@ -41,7 +41,7 @@ namespace CancionesWebApi.Helpers
                     // set clockskew to zero so tokens expire exactly at token expiration time (instead of 5 minutes later)
                     ClockSkew = TimeSpan.Zero
                 }, out SecurityToken validatedToken);
-                jwtToken = (JwtSecurityToken)validatedToken;
+                var jwtToken = (JwtSecurityToken)validatedToken;
                 var userId = int.Parse(jwtToken.Claims.First(x => x.Type == "id").Value);
 
                 // attach user to context on successful jwt validation
